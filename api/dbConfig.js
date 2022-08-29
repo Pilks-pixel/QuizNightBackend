91b77f554e8be2988795232f5ae451971633615a
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2022-08-29 17:27:36 +0200

    config

--- api/dbConfig.js
+++ api/dbConfig.js
@@ -1,11 +1,12 @@
 const { MongoClient } = require('mongodb')
 
-  const connectionUrl = process.env.CONNECTION_URL
-  const dbName = process.env.DATABASE_NAME
+  // const connectionUrl = process.env.CONNECTION_URL
+ const uri = process.env.MONGODB_URI;
+  const dbName = process.env.DATABASE_NAME;
 
 
 const init = async () => {
-  let client = await MongoClient.connect(connectionUrl)
+  let client = await MongoClient.connect(uri)
   // return MongoClient.connect(connectionUrl).then((client) => {
   //   db = client.db(dbName)
 
